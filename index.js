e1ee84f29c5d0e1b2145de468341ac1df55aac43
Belfort717 <145254955+Belfort717@users.noreply.github.com>
2024-03-06 18:44:41 +0000

    This code defines a helper function called "multiplyByTwo"

---
+++ index.js
@@ -0,0 +1,11 @@
+// Helper function
+function multiplyByTwo(number) {
+    return number * 2;
+  }
+  
+  // Main code
+  let inputNumber = 5;
+  let result = multiplyByTwo(inputNumber);
+  
+  console.log(`The result of multiplying ${inputNumber} by 2 is: ${result}`);
+  
